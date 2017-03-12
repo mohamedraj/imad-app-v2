6bd8cb0028159bfe54fff81145b3a611d51fd4aa
mohamedraj <mohamedraja2098@gmail.com>
2017-03-12 12:01:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.get('/article-one', function (req, res) {
 app.get('/article-two', function (req, res) {
   res.send('article two recorded and served here');
 });
+
 app.get('/article-three', function (req, res) {
   res.send('article three recorded and served here');
 });
